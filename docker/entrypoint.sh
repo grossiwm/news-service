50b9d884f9a86b51fffe0c5d6d526302dd77723f
Gabriel <grossiwm@gmail.com>
2022-09-15 00:52:48 -0300

    add datadog VM args

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,4 +1,8 @@
 service datadog-agent start
 
-java -javaagent:/var/app/dd-java-agent.jar -DnewsData.apiKey=${NEWS_DATA_API_KEY} \
+java -javaagent:/var/app/dd-java-agent.jar \
+-DnewsData.apiKey=${NEWS_DATA_API_KEY} \
+-Ddd.service=news-service \
+-Ddd.logs.injection=true \
+-Ddd.env=prod \
 -jar /var/app/newsapi-0.0.1-SNAPSHOT.jar
